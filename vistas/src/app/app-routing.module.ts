99697455dfb3efeb87f447e67a38b810e74aff01
johao70 <perlazajohao@gmail.com>
2019-08-06 18:22:12 -0500

    rutas home pedidos

--- vistas/src/app/app-routing.module.ts
+++ vistas/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'pedidos', component: PedidosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
