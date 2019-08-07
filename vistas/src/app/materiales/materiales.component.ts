5972234e103c0b2e6edb64304b4132269d536359
johao70 <perlazajohao@gmail.com>
2019-08-07 16:06:10 -0500

    materiales funcionando

--- vistas/src/app/materiales/materiales.component.ts
+++ vistas/src/app/materiales/materiales.component.ts
@@ -16,7 +16,12 @@ export class MaterialesComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
+    this.getDataMaterial()
+    this.getDataProveedor()
+    this.getDataNicho()
+
     this.formularioMateriales()
+
     this.table_header = [
       {
         id: 'N°',
@@ -30,6 +35,7 @@ export class MaterialesComponent implements OnInit {
       }
     ]
   }
+
   formularioMateriales(){
     this.materialesForm = this.formBuilder.group({
       id: [''],
@@ -38,42 +44,71 @@ export class MaterialesComponent implements OnInit {
       fecha_registro: ['',[Validators.required]],
       fecha_actualizacion: ['',[Validators.required]],
       precio: ['',[Validators.required]],
-      idNicho: ['',[Validators.required]],
-      idProveedor: ['',[Validators.required]],
-
+      idnicho: ['',[Validators.required]],
+      idproveedor: ['',[Validators.required]],
     });
   }
-//Pagina Principal
-respuesta:any[]
-  getDataPedidos = () => {
+
+  //Pagina Principal
+  respuestaMateriales:any[]
+
+  getDataMaterial = () => {
     let tabla = 'material'
     this.http.get<any>(environment.API_URL + `?tabla=${tabla}`)
     .subscribe(data => {
-        this.respuesta = data.datos
+        this.respuestaMateriales = data.datos
     })
   }  
+
   deleteDataTable = (value) => {
     let tabla = 'material'
     this.http.delete(environment.API_URL + `?tabla=${tabla}&id=${value}`)
     .subscribe( data => { })
     window.location.reload()
   }
+  //Pagina Principal
+
+  //MODAL NEW MATERIAL
+  nuevafecha = new Date()
+  fechaMaterial = this.nuevafecha.getDate() + "/" + (this.nuevafecha.getMonth() +1) + "/" + this.nuevafecha.getFullYear()
+
   postDataMateriales = () => {
     let nombre = this.materialesForm.get('nombre').value
     let descripcion = this.materialesForm.get('descripcion').value
     let fecha_registro = this.materialesForm.get('fecha_registro').value
     let fecha_actualizacion = this.materialesForm.get('fecha_actualizacion').value
     let precio = this.materialesForm.get('precio').value
-    let idNicho = this.materialesForm.get('idNicho').value
-    let idProveedor = this.materialesForm.get('idProveedor').value
+    let idnicho = this.materialesForm.get('idnicho').value
+    let idproveedor = this.materialesForm.get('idproveedor').value
     let returning
     let tabla = 'material'
-    let register = {tabla: tabla, datos: [{nombre: nombre, descripcion: descripcion, fecha_registro: fecha_registro, fecha_actualizacion: fecha_actualizacion, precio: precio, idNicho : idNicho, idProveedor:idProveedor}]}
+    let register = {tabla: tabla, datos: [{nombre: nombre, descripcion: descripcion, fecha_registro: this.fechaMaterial, fecha_actualizacion: this.fechaMaterial, precio: precio, idnicho: idnicho, idproveedor: idproveedor}]}
     this.http.post(environment.API_URL, register)
     .subscribe( data => { 
       returning = data
     })
-    console.log(returning)
     window.location.reload()
   }
+
+  respuestaProveedor: any[]
+
+  getDataProveedor = () => {
+    let tabla = 'proveedor'
+    this.http.get<any>(environment.API_URL + `?tabla=${tabla}`)
+    .subscribe(data => {
+      this.respuestaProveedor = data.datos
+    })
+  } 
+
+  respuestaNicho: any[]
+
+  getDataNicho = () => {
+    let tabla = 'nicho'
+    this.http.get<any>(environment.API_URL + `?tabla=${tabla}`)
+    .subscribe(data => {
+      this.respuestaNicho = data.datos
+    })
+  }  
+  //MODAL NEW MATERIAL
+
 }
