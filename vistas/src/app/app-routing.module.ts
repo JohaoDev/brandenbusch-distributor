9355769208337396e848a39ef8bb8d1693e697aa
johao70 <perlazajohao@gmail.com>
2019-08-06 20:30:15 -0500

    reclamos page and icons

--- vistas/src/app/app-routing.module.ts
+++ vistas/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
+import { ReclamosComponent } from './pedidos/reclamos/reclamos.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'pedidos', component: PedidosComponent },
+  { path: 'reclamos', component: ReclamosComponent },
 ];
 
 @NgModule({
